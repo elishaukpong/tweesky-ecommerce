31640ca4378295c890c48fc4492be8f2015c48f3
Elisha Ukpong <ishukpong418@gmail.com>
2025-05-15 23:29:33 +0100

    added force json middleware

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegistrationController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/register', RegistrationController::class)->name('auth.register');
